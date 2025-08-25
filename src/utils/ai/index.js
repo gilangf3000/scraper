4deda24f346f0f92f7cd38467b5f70bf638f3a7d
gilangf3000 <142327040+gilangf3000@users.noreply.github.com>
2025-08-25 19:16:57 +0700

    Update index.js

--- src/utils/ai/index.js
+++ src/utils/ai/index.js
@@ -3,7 +3,6 @@ const chatgemini = require('./chatgemini');
 const dalle = require('./dalle');
 const { penghitam } = require('./penghitam');
 const { ai4chat } = require('./ai4chat'); 
-const { gptoss } = require('./gptoss'); 
 const { deepseek } = require('./deepseek'); 
 
-module.exports = { turbochat, chatgemini, dalle, penghitam, ai4chat, gptoss, deepseek };
+module.exports = { turbochat, chatgemini, dalle, penghitam, ai4chat, deepseek };
